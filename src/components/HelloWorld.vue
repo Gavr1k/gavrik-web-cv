91856e12987c2ad24a4c2626541b4e2f2ce5a19c
Gavr1k <andrey.hauryk@gmail.com>
2024-06-22 17:38:34 +0300

    add three js sphere

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,132 +1,89 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br />
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener"
-        >vue-cli documentation</a
-      >.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router"
-          target="_blank"
-          rel="noopener"
-          >router</a
+  <div class="container">
+    <Renderer resize="window" orbit-ctrl>
+      <Camera :position="{ z: 70 }" />
+      <Scene>
+        <AmbientLight :intensity="0.5" />
+        <DirectionalLight :position="{ x: 50, y: 50, z: 50 }" :intensity="1" />
+        <DirectionalLight
+          :position="{ x: -50, y: 50, z: -50 }"
+          :intensity="1"
+        />
+        <Sphere
+          :radius="10"
+          :width-segments="64"
+          :height-segments="64"
+          :position="spherePosition"
+          ref="sphere"
         >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint"
-          target="_blank"
-          rel="noopener"
-          >eslint</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-unit-mocha"
-          target="_blank"
-          rel="noopener"
-          >unit-mocha</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-typescript"
-          target="_blank"
-          rel="noopener"
-          >typescript</a
-        >
-      </li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li>
-        <a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a>
-      </li>
-      <li>
-        <a href="https://forum.vuejs.org" target="_blank" rel="noopener"
-          >Forum</a
-        >
-      </li>
-      <li>
-        <a href="https://chat.vuejs.org" target="_blank" rel="noopener"
-          >Community Chat</a
-        >
-      </li>
-      <li>
-        <a href="https://twitter.com/vuejs" target="_blank" rel="noopener"
-          >Twitter</a
-        >
-      </li>
-      <li>
-        <a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a>
-      </li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li>
-        <a href="https://router.vuejs.org" target="_blank" rel="noopener"
-          >vue-router</a
-        >
-      </li>
-      <li>
-        <a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-devtools#vue-devtools"
-          target="_blank"
-          rel="noopener"
-          >vue-devtools</a
-        >
-      </li>
-      <li>
-        <a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener"
-          >vue-loader</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-          rel="noopener"
-          >awesome-vue</a
-        >
-      </li>
-    </ul>
+          <LambertMaterial
+            :color="'#ff7f00'"
+            :specular="'#ffffff'"
+            :shininess="100"
+          />
+        </Sphere>
+      </Scene>
+    </Renderer>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent, ref, onMounted } from 'vue';
+import {
+  Sphere,
+  Camera,
+  LambertMaterial,
+  DirectionalLight,
+  AmbientLight,
+  Renderer,
+  Scene,
+} from 'troisjs';
 
 export default defineComponent({
   name: 'HelloWorld',
-  props: {
-    msg: String,
+  components: {
+    Sphere,
+    Camera,
+    LambertMaterial,
+    Renderer,
+    Scene,
+    AmbientLight,
+    DirectionalLight,
+  },
+  setup() {
+    const spherePosition = ref({ x: 0, y: 0, z: 0 });
+    let direction = -1;
+
+    const animate = () => {
+      spherePosition.value.y += direction;
+
+      if (spherePosition.value.y > 100 || spherePosition.value.y < -100) {
+        direction *= -1;
+      }
+
+      requestAnimationFrame(animate);
+    };
+
+    onMounted(() => {
+      animate();
+    });
+
+    return {
+      spherePosition,
+    };
   },
 });
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
+<style>
+html,
+body,
+#app,
+.container {
+  margin: 0;
   padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
 }
 </style>
